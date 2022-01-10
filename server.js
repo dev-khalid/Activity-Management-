8f210fa277ffd4a72ef8f561945f7c8fcdd3fd09
dev-khalid <khalid.hossain.akash@gmail.com>
2022-01-10 21:02:25 +0600

    First try with both frontend and backend if i fail i will deploy my frontend in netlify

--- server.js
+++ server.js
@@ -5,6 +5,16 @@ import mongodb from 'mongodb';
 import studyRoute from './routes/studyRoute.js';
 import cors from 'cors';
 import cookieParser from 'cookie-parser';
+import  path from 'path'; 
+
+
+// Serve static files from the React frontend app
+app.use(express.static(path.join(__dirname, '../frontend/build')));
+
+// AFTER defining routes: Anything that doesn't match what's above, send back index.html; (the beginning slash ('/') in the string is important!)
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname + '/../frontend/build/index.html'));
+});
 //DATABASE CONNECTION
 mongoose
   .connect(process.env.MONGO_URI, {
