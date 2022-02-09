67dc18ed3c2f1c23268db944833f7a726bc743f1
dev-khalid <khalid.hossain.akash@gmail.com>
2022-02-09 13:43:19 +0600

    no duplicacy

--- controllers/fcmTokenHandler.js
+++ controllers/fcmTokenHandler.js
@@ -3,8 +3,7 @@ import FcmToken from '../models/fcmTokenModel.js';
 
 export const storeFcmToken = expressAsyncHandler(async (req, res) => {
   const token = req.body;
-  const data = await FcmToken.create(token);
+  const exist = await FcmToken.find(token).count();
+  if (!exist) await FcmToken.create(token);
   res.status(201).end();
 });
-
-  
