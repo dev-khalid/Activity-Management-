4cc53e2130b8b058616ec114a47252db56ad590e
dev-khalid <khalid.hossain.akash@gmail.com>
2022-02-27 22:52:45 +0600

    updated the target page with recent dates functionality

--- controllers/targetHandler.js
+++ controllers/targetHandler.js
@@ -37,18 +37,18 @@ export const getTarget = asyncHandler(async (req, res, next) => {
   const { page, userId } = req.params;
   const data = await Target.find({ userId, accomplished: false })
     .skip((page - 1) * process.env.DOCUMENTS_PER_PAGE)
-    .limit(process.env.DOCUMENTS_PER_PAGE);
+    .limit(process.env.DOCUMENTS_PER_PAGE)
+    .sort('deadline')
+    ;
   res.status(200).json(data);
 });
 
 export const getAllTargets = asyncHandler(async (req, res) => {
   const { userId } = req.params;
   const { accomplished } = req.query;
-  console.log(accomplished);
   const data = await Target.find({
     userId,
     accomplished,
   }).countDocuments();
-  console.log(data);
   res.json(data);
 });
